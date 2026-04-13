abd86214ecfcbda1bba47853460eac72cd70d822
pinchas lifshitz <pinchas.yesodot@gmail.com>
2026-04-13 15:35:35 +0300

    feat: implement supplier interface and mongoose model

---
+++ src/models/Supplier.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+import { ISupplier } from "../interfaces/supplier";
+
+const SupplierSchema = new Schema<ISupplier>({
+    name: { type: String,
+         required: true,
+         unique: true,
+         index: true 
+        },
+        items:[{
+           itemName: { type: String, required: true },
+           price: { type: Number, required: true ,min: 0 }
+        }]
+    },{timestamps: true});
+
+export const Supplier = model<ISupplier>("Supplier", SupplierSchema);
